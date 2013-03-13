89602706943fe32e4142c9dbe41f9e52cb2fa7d2
Alexandre Kandalintsev <spam@messir.net>
2013-03-14 00:06:29 +0100

    I hate python for not checking meaningless expressions.

--- resolver.py
+++ resolver.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 import socket
 import random
 try:
@@ -20,14 +22,15 @@ def resolve(addr, v4only=False, v6only=False):
   proto = socket.SOL_TCP
   family = 0
   if isaddr(addr):
+    #TODO: check if it is v4 or v6 addr
     return addr
 
   assert not (v4only and v6only), \
     "either v4only or v6nly"
   if v4only:
-    family == socket.AF_INET
+    family = socket.AF_INET
   elif v6only:
-    family == socket.AF_INET6
+    family = socket.AF_INET6
   r = socket.getaddrinfo(addr, 6666, family=family, proto=proto)
   return random.choice(r)[4][0]
 
@@ -38,3 +41,10 @@ def r4(addr):
 
 def r6(addr):
   return resolve(addr, v6only=True)
+
+if __name__ == '__main__':
+  print("IPv6 address of kliga.ru is ...", end=' ')
+  try:
+    print(r6("kliga.ru"))
+  except socket.gaierror:
+    print("cant resolve it :(")
