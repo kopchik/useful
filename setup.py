e4cac96d02f1cdacc72af3d42d5b2aec08982e1a
Alexandre Kandalintsev <spam@messir.net>
2013-05-03 16:46:35 +0200

    version bump

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name='exeutilz',
-      version='1.0', #TODO: remove dup version from __init__.py
+      version='1.1',
       author="Kandalintsev Alexandre",
       author_email='spam@messir.net',
       license="GPLv3",
